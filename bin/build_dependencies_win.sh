0570caf5f976066b6e5858249eef165148f374b6
sukuldhoka <ruchitshrestha98@gmail.com>
2022-09-21 23:56:09 -0400

    Update build_dependencies_win.sh

--- bin/build_dependencies_win.sh
+++ bin/build_dependencies_win.sh
@@ -174,10 +174,10 @@ cd ..
 #                                                                           #
 # ------------------------------------------------------------------------- #
 
-# curl -O -L https://github.com/fredrik-johansson/arb/archive/refs/tags/$ARBVER.tar.gz
-# mv $ARBVER.tar.gz arb-$ARBVER.tar.gz
-# tar xf arb-$ARBVER.tar.gz
-cd $GITHUB_WORKSPACE/arb
+curl -O -L https://github.com/fredrik-johansson/arb/archive/refs/tags/$ARBVER.tar.gz
+mv $ARBVER.tar.gz arb-$ARBVER.tar.gz
+tar xf arb-$ARBVER.tar.gz
+cd arb-$ARBVER
   ./configure --prefix=$PREFIX\
     --with-flint=$PREFIX\
     $FLINTARB_WITHGMP\
